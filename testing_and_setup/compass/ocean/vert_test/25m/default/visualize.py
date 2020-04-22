1f47c73f3b77ba2263ddd5dd3d61d8bd75d5c3b2
Jamil Gafur <jamilg@gr-fe1.lanl.gov>
2020-04-22 12:02:15 -0600

    updated code

--- testing_and_setup/compass/ocean/vert_test/25m/default/visualize.py
+++ testing_and_setup/compass/ocean/vert_test/25m/default/visualize.py
@@ -21,33 +21,11 @@ def main():
 
   data = xr.open_dataset(filename)
 
-
- 
   xCell_unique = np.unique(data.xCell.values)
   zMid_unique  = np.unique(data.zMid.values)
   tracer1 = data.tracer1.values[0,:100,:]
-  
-  # 
-  #
-  #
 
-  bottomDepth = data.bottomdepth...
-  config_vertical_advection_layer_thickness = something
-  distance_traveled = runtime * velocity
 
-  topofBox = bottomDepth[0] - config_vertical_advection_layer_thickness - distance_traveled
-  bottomBox = bottomDepth - distance_traveled
-
-  tracerExact = np.zeros_like(tracer1)
-
-  for k in range(len(zMid_unique)):
-    if zMid_unique[k] >= topOfBox and zMid_unique <= bottomOfBox:
-       tracerExact[:,k] = 1.0  
-
-  # to get rmse
-  # rmse( tracerExact, data.tracer1.values )
-
-  
   plt.contourf(xCell_unique[:100], zMid_unique[:100], tracer1)
   plt.savefig("t.png")
 
