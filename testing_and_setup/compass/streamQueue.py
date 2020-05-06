d3c82e46ef7ca0e1abed2846475ab2781acc22e3
Jamil Gafur <jamilg@gr-fe2.lanl.gov>
2020-05-05 18:57:46 -0600

    temp stream example

---
+++ testing_and_setup/compass/streamQueue.py
@@ -0,0 +1,39 @@
+from multiprocessing import Process
+import time
+import subprocess
+available = 40
+tasks = [10,10,20,20,10]
+
+running = []
+index = 0
+
+def task(message):
+  time.sleep(1)
+  print(message)
+
+while True:
+  print("current index : {}".format(index))
+  print("available processes: {}".format(available))
+  if index >= len(tasks):
+    break ;
+
+  if available >= tasks[index]:
+    print("we have {} available, current task uses {} added to queue".format(available, tasks[index]))
+    available = available - tasks[index]
+    command = "echo available {}, used {}".format(available, tasks[index])
+    proc = Process(target=task, args=(command,))
+    running.append([proc, tasks[index]])
+    proc.start()
+    index = index + 1
+  else:
+    print("not enough procs waiting till some finish")
+    print("\tavailable: {}\n\tindex: {}\n\tnext task: {}".format(available, index, tasks[index]))
+    while True:
+      if available >= tasks[index]:
+        break
+      else:
+        for proc in running:
+          proc[0].join(timeout=0)
+          if not proc[0].is_alive():
+            available += proc[1]
+
